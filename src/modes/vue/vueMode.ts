c637f980230397904f5cdf077c3dcfe677bf24c3
Peng Lyu <penn.lv@gmail.com>
2018-04-24 17:09:36 -0700

    upstream. remove duplicate bracket in scaffold completion

--- src/modes/vue/vueMode.ts
+++ src/modes/vue/vueMode.ts
@@ -2,14 +2,24 @@ import { LanguageMode } from '../languageModes';
 import { doScaffoldComplete } from './scaffoldCompletion';
 
 export function getVueMode(): LanguageMode {
-  return {
-    getId() {
-      return 'vue';
-    },
-    doComplete(document, position) {
-      return doScaffoldComplete();
-    },
-    onDocumentRemoved() {},
-    dispose() {}
-  };
+	return {
+		getId() {
+			return 'vue';
+		},
+		doComplete(document, position) {
+			const offset = document.offsetAt(position);
+			const text = document.getText().slice(0, offset);
+			const needBracket = /<\w*$/.test(text);
+			const ret = doScaffoldComplete();
+			// remove duplicate <
+			if (needBracket) {
+				ret.items.forEach(item => {
+					item.insertText = item.insertText!.slice(1);
+				});
+			}
+			return ret;
+		},
+		onDocumentRemoved() { },
+		dispose() { }
+	};
 }
